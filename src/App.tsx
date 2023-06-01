0066bb85880ac91f93404c791e29372ae832c2ff
shivang <shivang.sharma11@pm.me>
2023-06-01 15:00:21 +0530

    initial commit with landing page

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function LandingPage() {
+  const buttonStyle = {
+    marginRight: "10px",
+    padding: '10px 20px',
+    fontSize: '16px',
+  };
+
+  return (
+    <div className="App">
+      <h1 style={{ fontFamily: "Arial", fontSize: "32px" }}>
+        Welcome to GST e-filing!
+      </h1>
+      <p style={{ fontFamily: "Verdana", fontSize: "18px" }}>
+        File your GST returns online with ease.
+      </p>
+      <p style={{ fontFamily: "Verdana", fontSize: "18px" }}>
+        Stay compliant and save time by using our platform.
+      </p>
+      <p style={{ fontFamily: "Verdana", fontSize: "18px" }}>
+        Get started today!
+      </p>
+      <button style={buttonStyle}>Client</button>
+      <button style={buttonStyle}>Admin</button>
+    </div>
+  );
+}
+
+export default LandingPage;
